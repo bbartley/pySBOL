7dbbda249be8dc08f33047b37dc584c376f15cee
babartley <bartleyba@sbolstandard.org>
2014-12-29 13:23:48 -0800

    Updated documentation of namespaces inside pySBOL package. Updated href on mainpage.

--- sbol/sbol.py
+++ sbol/sbol.py
@@ -1,9 +1,18 @@
-## @file sbol.py
-# Implements a high-level, Pythonic interface for the SWIG-Python classes in libsbol
-#
-# @package sbol
+## @package sbol
 # A Python wrapper for libSBOLc, a module for reading, writing, and constructing
 # genetic designs according to the standardized specifications of the Synthetic Biology Open Language
+#
+# @file sbol.py
+# Implements a high-level, Pythonic interface for the SWIG-Python classes in libsbol
+#
+# @namespace sbol.sbol
+# High level wrappers for libSBOLc
+#
+# @namespace sbol.libsbol
+# Low level SWIG-Python wrappers for libSBOLc
+#
+# @namespace sbol.sbol_test
+# Unit tests
 
 import libsbol
 import sys
